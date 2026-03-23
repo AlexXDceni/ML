7f2e7e8f08c973ba619a8f11a5ac63339ddb8c3f
AlexXDceni <cenariualexandru46@gmail.com>
2026-03-23 19:37:41 +0200

    Evolve generation

--- main.cpp
+++ main.cpp
@@ -13,8 +13,10 @@ string TARGET_FILE = "targets.in";
 //ifstream fout ("ml.out");
 
 double BIAS = 0.01;
+double learning_rate = 0.03;
 
-double get_random_weight(){
+
+double get_random(){
 
     static mt19937 gen(random_device{}());
     return uniform_real_distribution<double>{-1, 1}(gen);
@@ -55,7 +57,7 @@ public:
     {   
         value = 0 ;
         for (auto it = 0; it != numNodesNextLayer; ++it){
-            weights.push_back( get_random_weight() );
+            weights.push_back( get_random() );
         }
     }
 };
@@ -88,7 +90,7 @@ public:
         bias = BIAS;
         for (int i = 0; i < numNodesNextLayer; ++i)
         { 
-            weights.push_back(get_random_weight());
+            weights.push_back(get_random());
         }
     }
 };
@@ -149,6 +151,8 @@ public:
     vector<Hidden_Layer> hidden;
     Output_Layer output;
 
+    // Initiate
+
     Network(vector<int> list) : sizes(list) {
         init(); 
     }
@@ -181,6 +185,47 @@ public:
         output = Output_Layer(output_nodes);
     }
 
+
+    // Read
+
+    vector<double> readInput(){
+
+        ifstream fin (INPUT_FILE);
+        vector<double> inputValues;
+        double tmp;
+        while(fin>>tmp){
+            inputValues.push_back(tmp);
+        }
+        fin.close();
+
+        if(inputValues.empty()) {
+            cerr << "Error: Input values are empty." << endl;
+            return {-1};
+        }
+
+        return inputValues;
+    }
+
+    vector<double> readTarget(){
+
+        ifstream fin (TARGET_FILE);
+        vector<double> targetValues;
+        double tmp;
+        while(fin>>tmp){
+            targetValues.push_back(tmp);
+        }
+        fin.close();
+
+        if(targetValues.empty()) {
+            cerr << "Error: Target values are empty." << endl;
+            return {-1};
+        }
+
+        return targetValues;
+    }
+
+    // Save/Load
+    
     void saveModel(string filename) {
         ofstream fout(filename);
         if (!fout.is_open()) {
@@ -263,95 +308,14 @@ public:
         cout << "Neural modal loaded from: " << filename << "!" << endl;
 }
 
+    // Display
+
     void printInfo() {
     cout << "Input Layer: " << input.nodes.size() << " nodes" << endl;
     for(int i = 0; i < hidden.size(); i++) {
         cout << "Hidden Layer " << i+1 << ": " << hidden[i].nodes.size() << " nodes" << endl;
     }
     cout << "Output Layer: " << output.nodes.size() << " nodes" << endl;
-    }
-
-    void feedZero() {
-
-        for( auto &layer : hidden ){
-            for( auto &node : layer.nodes ){
-                node.value = 0;
-            }
-        }
-        for( auto &node : output.nodes  ){
-            node.value = 0;
-        }
-    }
-
-    void feedForward(){
-
-        feedZero();
-
-        ifstream fin (INPUT_FILE);
-
-        for( auto &node : input.nodes ){
-            if (!fin.is_open()) {
-                cerr << "Error: Couldn't open theile named ml.in!" << '\n';
-                return; 
-            }
-            
-            if (!(fin >> node.value)) { 
-                cerr << "Error: Couldn't read input values." << endl;
-                return; 
-            }
-        }
-
-        fin.close();
-
-        bool areHiddenLayers = (hidden.size() > 0) ? 1 : 0;
-
-
-        // I-O 
-
-        if(!areHiddenLayers) {
-
-            for(int j = 0; j < output.nodes.size(); ++j) {
-                for(int i = 0; i < input.nodes.size(); ++i) {
-                    output.nodes[j].value += input.nodes[i].value * input.nodes[i].weights[j];
-                } 
-            output.nodes[j].value = RELU(output.nodes[j].value + output.nodes[j].bias);
-            }
-        }
-        
-        else {
-            
-
-            // I - L1
-
-            for(int j = 0; j < hidden[0].nodes.size(); ++j) {
-                for(int i = 0; i < input.nodes.size(); ++i) {
-                    hidden[0].nodes[j].value += input.nodes[i].value * input.nodes[i].weights[j];
-                } 
-            hidden[0].nodes[j].value = RELU(hidden[0].nodes[j].value + hidden[0].nodes[j].bias);
-            }
-            
-            
-            // L1 - Ln
-
-            for(int k = 0 ; k < hidden.size()-1; ++k )
-                for(int j = 0; j < hidden[k+1].nodes.size(); ++j) {
-                    for(int i = 0; i < hidden[k].nodes.size(); ++i) {
-                        hidden[k+1].nodes[j].value += hidden[k].nodes[i].value * hidden[k].nodes[i].weights[j];
-                    } 
-                hidden[k+1].nodes[j].value = RELU(hidden[k+1].nodes[j].value + hidden[k+1].nodes[j].bias);
-                }
-        
-            
-            // Ln - O
-
-            for(int j = 0; j < output.nodes.size(); ++j) {
-                for(int i = 0; i < hidden[hidden.size()-1].nodes.size(); ++i) {
-                    output.nodes[j].value += hidden[hidden.size()-1].nodes[i].value * hidden[hidden.size()-1].nodes[i].weights[j];
-                } 
-            output.nodes[j].value = RELU(output.nodes[j].value + output.nodes[j].bias);
-            }
-        
-        }
     }
 
     void printResults() {
@@ -362,7 +326,7 @@ public:
         cout << "------------------------" << endl;
 }
 
-    void displayFullNetwork() {
+    void displayFullNetwork(const vector<double>& targets) {
     cout << "\n================ Neural Network ================" << endl;
 
 
@@ -394,28 +358,19 @@ public:
     }
 
     cout <<"\n[LOSS]\n";
-    cout<<"  "<<calculateCost()<<'\n';
+    cout<<"  "<<calculateCost(targets)<<'\n';
     cout << "====================================================" << endl;
 }
 
-    double calculateCost() {
 
-        vector<double> targets;
-        ifstream target_in (TARGET_FILE);
-        
-        double tmp;
-        while(target_in>>tmp){
-            targets.push_back(tmp);
-        }
-        
+    // Calculate
 
+    double calculateCost(const vector<double>& targets) { 
         if (targets.size() != output.nodes.size()) {
-            cerr << "Error: Invalid number of outputs." << endl;
-            return -1;
+            return -1; 
         }
 
         double totalError = 0;
-
         for (int i = 0; i < output.nodes.size(); ++i) {
             double error = targets[i] - output.nodes[i].value;
             totalError += error * error; 
@@ -424,6 +379,122 @@ public:
         return totalError / output.nodes.size();
 }
 
+    void feedZero() {
+
+        for( auto &layer : hidden ){
+            for( auto &node : layer.nodes ){
+                node.value = 0;
+            }
+        }
+        for( auto &node : output.nodes  ){
+            node.value = 0;
+        }
+    }
+
+    void feedForward(const vector<double>& inputValues){
+
+        feedZero();
+
+        if (inputValues.size() != input.nodes.size()) {
+            cerr << "Error: Input size mismatch!" << endl;
+            return;
+        }   
+
+        for (int i = 0; i < input.nodes.size(); ++i) {
+            input.nodes[i].value = inputValues[i];
+        }
+
+        bool areHiddenLayers = (hidden.size() > 0) ? 1 : 0;
+
+
+        // I-O 
+
+        if(!areHiddenLayers) {
+
+            for(int j = 0; j < output.nodes.size(); ++j) {
+                for(int i = 0; i < input.nodes.size(); ++i) {
+                    output.nodes[j].value += input.nodes[i].value * input.nodes[i].weights[j];
+                } 
+            output.nodes[j].value = SIGMOID(output.nodes[j].value + output.nodes[j].bias);
+            }
+        }
+        
+        else {
+            
+
+            // I - L1
+
+            for(int j = 0; j < hidden[0].nodes.size(); ++j) {
+                for(int i = 0; i < input.nodes.size(); ++i) {
+                    hidden[0].nodes[j].value += input.nodes[i].value * input.nodes[i].weights[j];
+                } 
+            hidden[0].nodes[j].value = SIGMOID(hidden[0].nodes[j].value + hidden[0].nodes[j].bias);
+            }
+            
+            
+            // L1 - Ln
+
+            for(int k = 0 ; k < hidden.size()-1; ++k )
+                for(int j = 0; j < hidden[k+1].nodes.size(); ++j) {
+                    for(int i = 0; i < hidden[k].nodes.size(); ++i) {
+                        hidden[k+1].nodes[j].value += hidden[k].nodes[i].value * hidden[k].nodes[i].weights[j];
+                    } 
+                hidden[k+1].nodes[j].value = SIGMOID(hidden[k+1].nodes[j].value + hidden[k+1].nodes[j].bias);
+                }
+        
+            
+            // Ln - O
+
+            for(int j = 0; j < output.nodes.size(); ++j) {
+                for(int i = 0; i < hidden[hidden.size()-1].nodes.size(); ++i) {
+                    output.nodes[j].value += hidden[hidden.size()-1].nodes[i].value * hidden[hidden.size()-1].nodes[i].weights[j];
+                } 
+            output.nodes[j].value = SIGMOID(output.nodes[j].value + output.nodes[j].bias);
+            }
+        
+        }
+    }
+
+
+    // Learning
+    
+    void mutate(double rate){
+
+        for( auto &node : input.nodes)
+            for( auto &w : node.weights)
+                w += get_random() * rate;
+
+        for( auto &layer : hidden )
+            for( auto &node : layer.nodes){
+                node.bias += get_random() * rate;
+                for( auto &w : node.weights )
+                    w += get_random() * rate;
+            }
+        
+        for( auto &node : output.nodes )
+            node.bias += get_random() * rate;
+    }
+
+    void evolve(int numGen, double rate, const vector<double> targets, const vector<double> inputs){
+
+        this->feedForward(inputs);
+        double best_loss = this->calculateCost(targets);
+
+        for( int i = 1; i <= numGen ; ++ i ) {
+            
+            Network child = *this;
+            child.mutate(rate);
+            child.feedForward(inputs);
+            double new_loss = child.calculateCost(targets);
+
+            if(new_loss < best_loss){
+                *this = child;
+                best_loss = new_loss;
+                
+                cout<<"Generation: " << i<< " | Loss: " << best_loss<<'\n';
+            }
+        }
+    }
 };
 
 Network create_NN(){
@@ -459,11 +530,27 @@ Network create_NN(){
 
 int main()
 {
-    Network nn = Network({2,2});
+    Network nn = Network({2,8,7,5,2});
+    
+    vector<double> inputs = nn.readInput();
+    vector<double> targets = nn.readTarget();
 
-    nn.feedForward();
 
-    nn.displayFullNetwork();
+    cout << "--- Before ---" << endl;
+    nn.feedForward(inputs);
+    nn.printResults();
+
+    
+    cout << "\nBetween..." << endl;
+    nn.evolve(10000, 0.05, targets, inputs);
+
+
+    cout << "\n--- After ---" << endl;
+    nn.feedForward(inputs);
+    nn.printResults();
+    
+    
+    //nn.saveModel("trained_brain.txt");
 
     return 0;
 }
