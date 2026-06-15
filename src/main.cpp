d1fbfa25d3467d5cf2023ab96aeceb539ce689dd
AlexXDceni <cenariualexandru46@gmail.com>
2026-06-15 15:21:44 +0300

    Refactor file path definitions in main.cpp for improved readability and maintainability

--- src/main.cpp
+++ src/main.cpp
@@ -21,14 +21,22 @@ int main( int argc , char* argv[] )
     // Model metadata
     const string model_name = "test_model";
 
+
+    // Directries
+    const string MODELS_DIR = "../models/";
+    const string DATA_DIR = "../data/";
+    const string IO_DIR = DATA_DIR + "io/";
+    const string MNIST_DIR = DATA_DIR + "mnist/";
+    const string IMAGES_DIR = DATA_DIR + "images/";
+
     // Files
-    const string SAVE_FILE = "../models/" + model_name + ".json";
-    const string INPUT_FILE = "../data/io/inputs.in";
-    const string TARGET_FILE = "../data/io/targets.in";
-    const string TRAINING_MINST_INPUTS_FILE = "../data/mnist/train-images.idx3-ubyte";
-    const string TRAINING_MINST_LABELS_FILE = "../data/mnist/train-labels.idx1-ubyte";
-    const string MINST_INPUTS_FILE = "../data/mnist/t10k-images.idx3-ubyte";
-    const string MINST_LABELS_FILE = "../data/mnist/t10k-labels.idx1-ubyte";
+    const string SAVE_FILE = MODELS_DIR + model_name + ".json";
+    const string INPUT_FILE = IO_DIR + "inputs.in";
+    const string TARGET_FILE = IO_DIR + "targets.in";
+    const string TRAINING_MINST_INPUTS_FILE = MNIST_DIR + "train-images.idx3-ubyte";
+    const string TRAINING_MINST_LABELS_FILE = MNIST_DIR + "train-labels.idx1-ubyte";
+    const string MINST_INPUTS_FILE = MNIST_DIR + "t10k-images.idx3-ubyte";
+    const string MINST_LABELS_FILE = MNIST_DIR + "t10k-labels.idx1-ubyte";
 
 
     // Training parameters
@@ -37,8 +45,8 @@ int main( int argc , char* argv[] )
 
     const int displayInterval = 1;
     const bool saveOnInterrupt = true;
-    const int checkpointInterval = -1;
-    const int mnist_max_samples = -1;
+    const int checkpointInterval = -1;  // -1 means no checkpoints, otherwise it will save the model every checkpointInterval epochs
+    const int mnist_max_samples = -1;   // -1 means load all samples, otherwise it will load only the specified number of samples from the mnist dataset
 
     const int task = -1;             
     // 0 - Create
@@ -104,34 +112,36 @@ int main( int argc , char* argv[] )
                     cout << val << " ";
                 cout << "-> AI says: " << prediction[0] << endl;
             }
-        break;
+            break;
         }
 
 
-        case 4: // Test mnist
-            {
-                NeuralNetwork nn = NeuralNetwork( nn.get_params(SAVE_FILE) );
-                nn.load_model(SAVE_FILE);
-                const int max_samples = 1000; 
-                const int tests_number = 1000;
-                nn.MnistTest(MINST_INPUTS_FILE, MINST_LABELS_FILE, max_samples, tests_number);
-                break;
-            }
-        case 5: // Learn mnist
-            {
-                NeuralNetwork nn = NeuralNetwork( nn.get_params(SAVE_FILE) );  
-                nn.signalHandler(saveOnInterrupt);
-                nn.load_model(SAVE_FILE);
-                nn.loadMnist(TRAINING_MINST_INPUTS_FILE, TRAINING_MINST_LABELS_FILE, mnist_max_samples); 
-                nn.backpropagate(learning_rate, epochs, checkpointInterval, displayInterval); 
-                nn.save_model(SAVE_FILE);
-                break;
-            }
-        default:
-            {
+    case 4: // Test mnist
+        {
+            NeuralNetwork nn = NeuralNetwork( nn.get_params(SAVE_FILE) );
+            nn.load_model(SAVE_FILE);
+            const int max_samples = 1000; 
+            const int tests_number = 1000;
+            nn.MnistTest(MINST_INPUTS_FILE, MINST_LABELS_FILE, max_samples, tests_number);
+            break;
+        }
+
+    case 5: // Learn mnist
+        {
+            NeuralNetwork nn = NeuralNetwork( nn.get_params(SAVE_FILE) );  
+            nn.signalHandler(saveOnInterrupt);
+            nn.load_model(SAVE_FILE);
+            nn.loadMnist(TRAINING_MINST_INPUTS_FILE, TRAINING_MINST_LABELS_FILE, mnist_max_samples); 
+            nn.backpropagate(learning_rate, epochs, checkpointInterval, displayInterval); 
+            nn.save_model(SAVE_FILE);
+            break;
+        }
+
+    default:
+        {
             cout<<"No test case selected, change task value.";
             break;
-            }
+        }
     }   
     return 0;
 }
